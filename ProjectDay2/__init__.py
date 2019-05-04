232ff0b11a674dba2613b17e272abb0226631eb6
Wawan Ridwan <wawan.ridone@gmail.com>
2019-05-04 10:48:36 +0700

    bootcampday2 wawanridwan

---
+++ ProjectDay2/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from flask_script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+   return render_template('index.html')
